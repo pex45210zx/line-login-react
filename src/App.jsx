51b25fab902e16d36ef53495477aacd248db34ac
Siravat matarach <pex_der@outlook.co.th>
2023-09-07 14:24:29 +0700

    send message

--- src/App.jsx
+++ src/App.jsx
@@ -16,6 +16,9 @@ const LiffLoginExample = () => {
         const profile = await liff.getProfile();
         setProfilePicture(profile.pictureUrl);
         setUserId(profile.userId);
+
+        // After the user is logged in, send a message to the user
+        sendMessageToUser(profile.userId);
       }
     } catch (error) {
       console.error('LIFF initialization failed:', error);
@@ -27,6 +30,39 @@ const LiffLoginExample = () => {
       liff.logout();
       setProfilePicture('');
       setUserId('');
+    }
+  };
+
+  const sendMessageToUser = async (userId) => {
+    try {
+      // Replace with your LINE Messaging API access token and your message content
+      const accessToken = 'yDmTxLHJQNxhaFUOCGbQD9k0z9W8GqB8wZ0gSEwzxXJRuZu//E97fIuQvEQBxSVQpp+2eubaXpy2xobdI9d6jivZIAz6k4Pah+sIIFYGOH3igxc1Z5pCGh9MEy+Vylkf+Y4aKgXUs1jEzHYj+Aj+dwdB04t89/1O/w1cDnyilFU=';
+      const message = 'Hello, this is a test message from your LINE OA!';
+
+      const response = await fetch('https://api.line.me/v2/bot/message/push', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${accessToken}`,
+        },
+        body: JSON.stringify({
+          to: userId,
+          messages: [
+            {
+              type: 'text',
+              text: message,
+            },
+          ],
+        }),
+      });
+
+      if (response.status === 200) {
+        console.log('Message sent successfully');
+      } else {
+        console.error('Failed to send message:', response.statusText);
+      }
+    } catch (error) {
+      console.error('Error sending message:', error);
     }
   };
 
